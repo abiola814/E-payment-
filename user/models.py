0b379537ce64f38995e2b2623e58b5eba781f3c1
Ajibade Yakub Adebare <ajibadeyakubadebare@gmail.com>
2022-10-09 21:57:34 +0100

    Modified the register view and profile model. Added dashboard back in

--- user/models.py
+++ user/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
@@ -6,10 +7,10 @@ import uuid
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
     full_name = models.CharField(max_length=2000, blank=True)
-    email = models.EmailField(max_length=2000, blank=True, null=True)
+    email = models.EmailField(max_length=2000, blank=True, null=True, unique=True)
     profile_image = models.ImageField(null=True, blank=True, upload_to = "profiles/", default = 'profiles/user-default.png')
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.full_name)
--- user/urls.py
+++ user/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 
 urlpatterns = [
-        path('', views.home, name ='landing'),
+        path('', views.home, name ='home'),
         path('register/', views.registeruser, name='register'),
         path('login/', views.loginPage, name='login'),
         path('logout/', views.logoutuser, name='logout'),
+        path('dashboard/', views.dashboard, name='dashboard'),
+        path('general/', views.general, name='general'),
     ]
