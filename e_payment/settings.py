10a903f0b8049c90593f7c801f6435718bdf9813
Your Name <you@youraddress.com>
2022-10-11 16:42:08 +0100

    ekiti

--- e_payment/settings.py
+++ e_payment/settings.py
@@ -145,7 +145,7 @@ EMAIL_USE_TLS = True
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_FROM_USER = "connecttechagency@gmail.com"
 
-EMAIL_USE_SSL = False
+EMAIL_USE_SSL = True
 
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_HOST_USER ="connecttechagency@gmail.com"
