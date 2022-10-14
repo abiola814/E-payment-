87d0f62c087d1cdef1288c3347d4570b975f1c64
Ajibade Yakub Adebare <ajibadeyakubadebare@gmail.com>
2022-10-14 23:01:37 +0100

    fd

--- e_payment/settings.py
+++ e_payment/settings.py
@@ -151,9 +151,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_HOST_USER="connecttechagency@gmail.com"
 # EMAIL_HOST_PASSWORD =env("EMAIL_HOST_PASSWORD")
 
-EMAIL_HOST_USER ="differencereal6@gmail.com"
-EMAIL_HOST_PASSWORD = "gnaiaaxhhqsskdrt"
-
+# EMAIL_HOST_USER ="differencereal6@gmail.com"
+# EMAIL_HOST_PASSWORD = ""
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
 
